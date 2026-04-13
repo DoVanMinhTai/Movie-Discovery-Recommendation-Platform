9ccee8f1b9bbacae0cfe64de7c539fbefaa111c7
dovanminhtai <dotai05678910@gmail.com>
2026-04-13 11:19:13 +0700

    fix: close missing curly brace in AuthController

--- movie-backend/src/main/java/nlu/fit/movie_backend/controller/AuthController.java
+++ movie-backend/src/main/java/nlu/fit/movie_backend/controller/AuthController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
-@CrossOrigin(origins = "*")
 public class AuthController {
     private final AuthService authService;
     private final JWTService jWTService;
